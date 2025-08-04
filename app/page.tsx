899c0e6f50cdb2d65b388e647bf768e4808049fe
AndreiBontea <fvndbsh@gmail.com>
2025-08-04 12:04:06 +0300

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import FaceScanner from "@/components/FaceScanner";
+
+export default function Home() {
+  return (
+    <main className="flex items-center justify-center min-h-screen bg-gray-100">
+      <FaceScanner />
+    </main>
+  );
+}
